9826715347b1a600afb07edd915bdc69d37e2055
v.arkhipenka <v.arkhipenka@dataduck.com>
2021-05-07 10:14:34 +0300

    get ready to code

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less'
+
+const App = () => {
+    return (
+        <div className="app">
+            Hello World, I'm Arkhip
+        </div>
+    );
+};
+
+export default App;
